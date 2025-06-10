1c1d8a0af2d1e9ffce9192272e133e04619a8f36
Leo2406Abi <lionelcolli4001@gmail.com>
2025-06-09 19:25:37 -0600

    Update views.py

--- tienda/views.py
+++ tienda/views.py
@@ -1,12 +1,11 @@
 from django.db import connection
 from django.shortcuts import render
-from .models import Producto
+from tienda.models import Producto
 
 def pagina_tienda(request):
     productos = Producto.objects.all()
     print("Productos encontrados:", productos.count())
     return render(request, 'tienda/productos.html', {'productos': productos})
-
 
 def detalle_producto(request):
     return render(request, 'tienda/detalle_producto.html')
