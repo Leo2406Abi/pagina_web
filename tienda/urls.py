bda3765c716e0fece1869299f3087a58d58e080e
Leo2406Abi <lionelcolli4001@gmail.com>
2025-06-09 19:26:12 -0600

    Update urls.py

--- tienda/urls.py
+++ tienda/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('tienda/', views.pagina_tienda, name='pagina_tienda'),
+    path('productos/', views.pagina_tienda, name='pagina_tienda'),
     path('detalle-producto/', views.detalle_producto, name='detalle_producto'),
     path('carrito/', views.carrito, name='carrito'),
     path('checkout/', views.checkout, name='checkout'),
