19932555c6fff67239ba72b5d207fa8caeb4fdb7
Leo2406Abi <lionelcolli4001@gmail.com>
2025-06-09 22:32:44 -0600

    Update urls.py

--- tienda/urls.py
+++ tienda/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('productos/', views.pagina_tienda, name='pagina_tienda'),
     path('agregar-carrito/', views.agregar_carrito, name='agregar_carrito'),
     path('carrito/', views.ver_carrito, name='ver_carrito'),
+    path("eliminar-del-carrito/", views.eliminar_del_carrito, name="eliminar_del_carrito"),
 ]
